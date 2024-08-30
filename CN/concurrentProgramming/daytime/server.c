ddccc083527dda8d21c3ab4cffec33cd2401b834
Vishesh <visheshvishu1@gmail.com>
2024-08-30 14:51:16 +0530

    concurrent programming

---
+++ CN/concurrentProgramming/daytime/server.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define PORT 8080
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket, max_sd, activity, sd;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE];
+    
+    fd_set readfds;
+    int client_socket[MAX_CLIENTS] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Daytime server is listening on port %d\n", PORT);
+
+    while (1) {
+
+        FD_ZERO(&readfds);
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+            if (sd > 0) {
+                FD_SET(sd, &readfds);
+            }
+            if (sd > max_sd) {
+                max_sd = sd;
+            }
+        }
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("Select error\n");
+        }
+        if (FD_ISSET(server_fd, &readfds)) {
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+                perror("Accept failed");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("New connection, socket fd: %d, IP: %s, Port: %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (client_socket[i] == 0) {
+                    client_socket[i] = new_socket;
+                    printf("Added to list of sockets as %d\n", i);
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = client_socket[i];
+
+            if (FD_ISSET(sd, &readfds)) {
+                int valread = read(sd, buffer, BUFFER_SIZE);
+                if (valread == 0) {
+                    getpeername(sd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+                    printf("Host disconnected, IP: %s, Port: %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+                    close(sd);
+                    client_socket[i] = 0;
+                } else {
+                    buffer[valread] = '\0'; 
+                    time_t now = time(NULL);
+                    struct tm *local = localtime(&now);
+
+                    char time_str[BUFFER_SIZE];
+                    snprintf(time_str, sizeof(time_str), "%02d:%02d:%02d %02d-%02d-%04d",
+                             local->tm_hour, local->tm_min, local->tm_sec,
+                             local->tm_mday, local->tm_mon + 1, local->tm_year + 1900);
+
+                    send(sd, time_str, strlen(time_str), 0);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
